793693632e4229c2e72ac112829b3fba3f7a3a25
Samuel Joseph <samuelj123@gmail.com>
2022-07-22 04:47:57 +0000

    Fixed the bug

--- src/components/ProjectArc.tsx
+++ src/components/ProjectArc.tsx
@@ -2,19 +2,15 @@ import * as d3 from 'd3';
 
 const ProjectArc = () => {
     const svg = d3.select('svg')
+        // .attr('className', 'arc')
         .append('g');
-    // const myArc = d3.arc()
-    //     .innerRadius(40)
-    //     .outerRadius(50)
-    //     .startAngle(100)
-    //     .endAngle(360);
-    svg.append('circle')
-        .attr('r', 5)
-        .attr('cx', 6)
-        .attr('cy', 6)
-        .attr('fill', 'red');
-    // svg.append('path')
-    //     .attr('d', myArc)
+    const myArc: d3.Arc<any, any> = d3.arc()
+        .innerRadius(40)
+        .outerRadius(50)
+        .startAngle(100)
+        .endAngle(360);
+    svg.append('path')
+        .attr('d', myArc)
     return (
         <svg></svg>
     )
